a28599ebd45eac0449da1b24dbc68e3fc3ebf2a7
chineduoscar <chinedunnadoziepatrick@gmail.com>
2026-07-13 14:47:22 +0100

    updating the routing

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -33,6 +33,7 @@ export default async function AdminLayout({
   children: React.ReactNode;
 }) {
   const user = await getUser();
+  console.log(user?.role);
 
   if (!user || user.role !== "admin") {
     redirect("/admin-login");
